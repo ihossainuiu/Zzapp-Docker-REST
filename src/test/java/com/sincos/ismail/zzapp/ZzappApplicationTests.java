ce9328d2593cd9afae71273d45759f9335ab3936
ihossain <ihossain.mac@gmail.com>
2019-11-30 16:37:39 +0600

    Add All

---
+++ src/test/java/com/sincos/ismail/zzapp/ZzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sincos.ismail.zzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
